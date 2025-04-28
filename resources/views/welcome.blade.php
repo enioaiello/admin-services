6aad9aa0647446111cd8bb9c73a004b290ddfca5
Enio Aiello <eaiello@edenschool.fr>
2025-04-28 11:56:40 +0200

    Mis à jour des pages de bienvenue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,22 @@
-{{--<!doctype html>--}}
-{{--<html lang="fr">--}}
-{{--    <head>--}}
-{{--        <meta charset="UTF-8">--}}
-{{--        <meta name="viewport" content="width=device-width, initial-scale=1.0">--}}
-{{--        <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
-{{--        <meta name="description" content="Admin Services est un site Internet permettant aux utilisateurs de publier des applications web et de les utiliser">--}}
-{{--        <title>Admin Services</title>--}}
-{{--        <link rel="stylesheet" href="{{ asset('css/components/welcome.css') }}">--}}
-{{--        <link rel="icon" href="{{ asset('favicon.ico') }}">--}}
-{{--        <!-- Métadonnées Open Graph -->--}}
-{{--        <meta property="og:title" content="Admin Services">--}}
-{{--        <meta property="og:description" content="Créez et utilisez des applications au format wep-app">--}}
-{{--        <meta property="og:image" content="https://exemple.com/image.jpg">--}}
-{{--        <meta property="og:url" content="https://exemple.com">--}}
-{{--        <meta property="og:type" content="website">--}}
-{{--    </head>--}}
-{{--    <body>--}}
-{{--        <div class="container">--}}
-{{--            <img src="{{ asset('images/logo.png') }}" alt="Logo">--}}
-{{--        </div>--}}
-{{--    </body>--}}
-{{--</html>--}}
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Admin Services est un site Internet permettant aux utilisateurs de publier des applications web et de les utiliser">
+    <title>Admin Services</title>
+    <link rel="stylesheet" href="{{ asset('css/components/welcome.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <!-- Métadonnées Open Graph -->
+    <meta property="og:title" content="Admin Services">
+    <meta property="og:description" content="Créez et utilisez des applications au format wep-app">
+    <meta property="og:image" content="{{ asset('images/banner.png') }}">
+    <meta property="og:url" content="https://exemple.com">
+    <meta property="og:type" content="website">
+</head>
+<body class="body">
 
-@include('layouts.header')
-
-<main>
-    <div class="banner">
-        <h1 class="title">Admin Services</h1>
-        <h2 class="subtitle">Toutes vos applications sur une plateforme.</h2>
-    </div>
-</main>
-
-@include('layouts.footer')
+</body>
+</html>
